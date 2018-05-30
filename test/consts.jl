9ddddf0ed35b514b9988dd317f2f567bb7790f88
Wu Sun <37353134+wusunlab@users.noreply.github.com>
2018-05-30 16:11:55 -0700

    TST: refactored TestConstants

---
+++ test/consts.jl
@@ -0,0 +1,27 @@
+module TestConstants
+
+using Base.Test
+using SoilTracers: Constants
+
+
+# test global constants
+
+@time @testset "Test physical constants" begin
+    @test typeof(Constants.T_0) <: AbstractFloat
+    @test isapprox(Constants.T_0, 273.15)
+
+    @test typeof(Constants.R) <: AbstractFloat
+    @test isapprox(Constants.R, 8.314, rtol=1e-3)
+
+    @test typeof(Constants.g) <: AbstractFloat
+    @test isapprox(Constants.g, 9.81, rtol=1e-3)
+
+    @test typeof(Constants.atm) <: AbstractFloat
+    @test isapprox(Constants.atm, 101325., rtol=1e-3)
+
+    @test typeof(Constants.M_w) <: AbstractFloat
+    @test isapprox(Constants.M_w, 18.015e-3, rtol=1e-3)
+end
+
+
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,15 +1,40 @@
-using SoilTracers
 using Base.Test
+using Compat  # for breaking changes in julia 0.7
+using SoilTracers
 
-println("Starting tests")
 
-@time @testset "Test physical constants" begin
-    include("test_consts.jl")
+anyerrors = false
+
+tests = ["consts.jl"]
+        #  "test_physchem/test_diffus.jl",
+        #  "test_physchem/test_solub.jl",
+        #  "test_physchem/test_water.jl",
+        #  "test_physchem/test_rxn.jl"]
+
+
+println("Running tests:")
+
+
+for t in tests
+    try
+        include(t)
+        println("\t\033[1m\033[32mPASSED\033[0m: $(t)")
+    catch e
+        global anyerrors = true
+        println("\t\033[1m\033[31mFAILED\033[0m: $(t)")
+        showerror(stdout, e, backtrace())
+    end
 end
+
 
 @time @testset "Test PhysChem functions" begin
     include("test_physchem/test_diffus.jl")
     include("test_physchem/test_solub.jl")
     include("test_physchem/test_water.jl")
     include("test_physchem/test_rxn.jl")
+end
+
+
+if anyerrors
+    throw(error("Tests failed"))
 end

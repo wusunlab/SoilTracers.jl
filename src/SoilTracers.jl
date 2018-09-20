86381976ceb3d296db65fa4269dea2fca15f2a0a
Wu Sun <37353134+wusunlab@users.noreply.github.com>
2018-09-19 21:30:34 -0700

    refactor for 1.0 - constants module

--- src/SoilTracers.jl
+++ src/SoilTracers.jl
@@ -1,14 +1,15 @@
-__precompile__()
-
 module SoilTracers
 
-include("consts.jl")  # Constants
-include("physchem/physchem.jl")  # PhysChem
-include("soilgrid/soilgrid.jl")  # SoilGrid
-include("tracers/tracers.jl")  # Tracers
+include("constants.jl")  # Constants
 
-# expose submodule public members to the main module namespace
-using SoilTracers.PhysChem
-using SoilTracers.SoilGrid
+# ====== refactoring =======
 
-end # module
+# include("physchem/physchem.jl")  # PhysChem
+# include("soilgrid/soilgrid.jl")  # SoilGrid
+# include("tracers/tracers.jl")  # Tracers
+
+# # expose submodule public members to the main module namespace
+# using SoilTracers.PhysChem
+# using SoilTracers.SoilGrid
+
+end  # module
